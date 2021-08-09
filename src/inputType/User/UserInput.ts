e726e9c329da24a077749d7eda217662a58325e9
qaboxletstest <qaboxletstest@gmail.com>
2021-08-09 11:44:34 +0530

    Input Validation

--- src/inputType/User/UserInput.ts
+++ src/inputType/User/UserInput.ts
@@ -1,14 +1,19 @@
-import { InputType } from "type-graphql";
-import { Field, Int } from "type-graphql";
+import { IsInt, Length, Max, Min } from "class-validator";
+import { Field, Int, InputType } from "type-graphql";
 
 @InputType()
 export class UserInput {
     @Field()
+    @Length(3, 10)
     firstName: string;
 
     @Field()
+    @Length(3, 10)
     lastName: string;
 
     @Field(() => Int)
+    @IsInt()
+    @Min(18)
+    @Max(60)
     age: number;
 }
--- src/inputType/User/UserUpdate.ts
+++ src/inputType/User/UserUpdate.ts
@@ -1,14 +1,20 @@
 import { InputType } from "type-graphql";
 import { Field, Int } from "type-graphql";
+import { IsInt, Length, Max, Min } from "class-validator";
 
 @InputType()
 export class UserUpdate {
     @Field(() => String, { nullable: true })
+    @Length(3, 10)
     firstName?: string;
 
     @Field(() => String, { nullable: true })
+    @Length(3, 10)
     lastName?: string;
 
     @Field(() => Int, { nullable: true })
+    @IsInt()
+    @Min(18)
+    @Max(60)
     age?: number;
 }
